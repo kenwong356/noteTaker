5c0dc762ba1fa0c023a452579d6994c6a527a7f2
Ken <kanney356@gmail.com>
2023-12-03 19:39:34 -0800

    fixed delete route

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
 
@@ -34,21 +34,14 @@ apiRoute.post('/', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
-apiRoute.delete(`/notes/:id`, (req, res) => {
-    if (req.params.id) {
-        const noteId = req.params.id;
 
-        for (let i = 0; i < noteData.length; i++) {
-            const currentNote = noteData[i];
-            const out = () => {
-            noteData[i].splice(i - 1, 1);
-            }
-            if (noteId === currentNote.id) {
-            out();
-            }
-        }
-    } else {
-        res.status(500).json('Delete request failed!')
-      }
-    });
+apiRoute.delete("/:id", async (req, res) => {
+    console.log("deleted");
+        const noteId = req.params.id;
+        let noteData = await readFromFile('./db/db.json')
+        let notes = (JSON.parse(noteData));
+       notes = notes.filter((note)=> note.id != noteId)
+       await writeToFile( 'db/db.json', notes);
+       res.json(notes);
+});
 module.exports = apiRoute;
